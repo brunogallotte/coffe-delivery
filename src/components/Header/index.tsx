d28e1cf5d4f94726ca9196bf4af95ab03c39dd5e
Bruno Gallotte <brunogallotte1@gmail.com>
2023-12-28 16:37:39 -0300

    Feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,17 @@
+import { HeaderContainer, BoxInfo, CartInfo } from "./styles";
+
+import logoCoffe from '../../assets/images/LogoCoffe.svg'
+import iconMap from '../../assets/icons/IconMap.svg'
+import iconCart from '../../assets/icons/CartIcon.svg'
+
 export function Header() {
     return(
-        <header>
-            teste
-        </header>
+        <HeaderContainer>
+            <img src={logoCoffe} />
+            <div className="wrapper">
+                <BoxInfo><img src={iconMap} />Porto Alegre, RS</BoxInfo>
+                <CartInfo><img src={iconCart} /></CartInfo>
+            </div>
+        </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
-    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 10rem;
+
+    .wrapper {
+        display: flex;
+        align-items: center;
+        gap: 0.75rem;
+    }
+`
+
+export const BoxInfo = styled.span`
+    display: flex;
+    padding: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    gap: 0.25rem;
+    border-radius: 6px;
+    color: ${props => props.theme.purpleDark};
+    background: ${props => props.theme.purpleLight};
+`
+
+export const CartInfo = styled(BoxInfo)`
+    background: ${props => props.theme.yellowLight};
 `
